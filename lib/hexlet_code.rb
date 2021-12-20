44bde7d3c9a201913ea7b5152285037d04467cd8
Anastasia <anastasia.zhura@gmail.com>
2021-12-20 18:01:52 +0300

    add tags generator logic

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -3,6 +3,29 @@
 require_relative "hexlet_code/version"
 
 module HexletCode
-  class Error < StandardError; end
-  # Your code goes here...
+  SINGLE_TAGS = %w[br hr img input].freeze
+  # class to generate HTML tag
+  class Tag
+    class << self
+      def build(tag, params = {})
+        tag_params = build_params(params)
+
+        return "<#{tag}#{tag_params}>#{yield}</#{tag}>" if block_given?
+
+        if SINGLE_TAGS.include?(tag)
+          "<#{tag}#{tag_params}>"
+        else
+          "<#{tag}#{tag_params}></#{tag}>"
+        end
+        # return "<#{tag}#{tag_params}>" if SINGLE_TAGS.include?(tag)
+        # "<#{tag}#{tag_params}></#{tag}>"
+      end
+
+      private
+
+      def build_params(params)
+        params.collect { |key, value| %( #{key}="#{value}") }.join
+      end
+    end
+  end
 end
